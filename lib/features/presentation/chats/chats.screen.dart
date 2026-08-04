bac41cc60e91f63be5e45227dc80193c1fc4ec2e
Kaikai-dev <jecalaycay17@gmail.com>
2026-08-05 01:50:19 +0800

    dotted border on "Your Story"

--- lib/features/presentation/chats/chats.screen.dart
+++ lib/features/presentation/chats/chats.screen.dart
@@ -1,6 +1,7 @@
 import 'package:chat_app/core/theme/theme.provider.dart';
 import 'package:chat_app/core/widgets/inputs/app_text_field.widget.dart';
 import 'package:chat_app/features/domain/extensions/build_context.dart';
+import 'package:dotted_border/dotted_border.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -42,8 +43,9 @@ class _ChatsScreenState extends ConsumerState<ChatsScreen> {
       ),
       body: SingleChildScrollView(
         child: Padding(
-          padding: const EdgeInsets.all(8.0),
+          padding: const EdgeInsets.all(20.0),
           child: Column(
+            spacing: 16,
             children: [
               AppTextField(
                 hintText: "Search Conversations",
@@ -51,6 +53,52 @@ class _ChatsScreenState extends ConsumerState<ChatsScreen> {
                   Icons.search
                 ),
               ),
+              SingleChildScrollView(
+                child: Row(
+                  children: [
+                    Column(
+                      spacing: 12,
+                      children: [
+                        Container(
+                          decoration: BoxDecoration(
+                            color: context.theme.inputDecorationTheme.hintStyle!.color!,
+                            borderRadius: BorderRadius.circular(100)
+                          ),
+                          child: DottedBorder(
+                            options: RoundedRectDottedBorderOptions(
+                              strokeWidth: 1.25,
+                              radius: Radius.circular(100),
+                              padding: EdgeInsets.all(20),
+                              color: context.theme.colorScheme.onSurface,
+                              dashPattern: [6, 2],
+                            ),
+                            child: Icon(
+                              Icons.add
+                            )
+                          ),
+                        ),
+                        Text(
+                          "Your Story",
+                          style: context.textTheme.bodySmall,
+                        )
+                      ],
+                    )
+                    // Container(
+                    //   padding: EdgeInsets.all(16),
+                    //   decoration: BoxDecoration(
+                    //     border: Border.all(
+                    //       width: 1,
+                    //       style: 
+                    //     ),
+                    //     borderRadius: BorderRadius.circular(100)
+                    //   ),
+                      // child: Icon(
+                      //   Icons.add
+                      // ),
+                    // )
+                  ],
+                ),
+              )
             ],
           ),
         ),
