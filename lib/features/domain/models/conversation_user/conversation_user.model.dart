927f8587e9bfc9db8777b078ae8e0f7d17d2cc3a
Kaikai-dev <jecalaycay17@gmail.com>
2026-08-19 02:57:43 +0800

    task: added userId, and last_read_at to ConversationUser model

--- lib/features/domain/models/conversation_user/conversation_user.model.dart
+++ lib/features/domain/models/conversation_user/conversation_user.model.dart
@@ -10,9 +10,10 @@ part 'conversation_user.model.g.dart';
 abstract class ConversationUser with _$ConversationUser {
   const factory ConversationUser({
     required int id,
+    @JsonKey(name: "user_id") required userId,
     @JsonKey(name: "conversation_id") required int conversationId,
     @Default(ConversationRole.member) ConversationRole? role,
-    @JsonKey(name: "last_read") required String lastRead,
+    @JsonKey(name: "last_read_at") required String? lastRead,
     @JsonKey(name: "is_muted") @Default(false) bool muted,
 
   }) = _ConversationUser;
